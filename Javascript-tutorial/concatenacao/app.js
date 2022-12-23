393606e35b450cafa56a41c4bec76c9267316782
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-12-23 11:48:53 -0300

    criação da pasta e aquivo concatenação/app.js.

---
+++ Javascript-tutorial/concatenacao/app.js
@@ -0,0 +1,22 @@
+//
+const firstName = 'Thais';
+const lastName = 'Sampaio';
+
+let fullName = `${firstName} ${lastName}`;
+
+console.log('hello there your full name is: ' + firstName + ' ' + lastName);
+console.log(`hello there your full name is: ${fullName}`);
+//
+const website = 'google';
+const url = 'http://www.' + website + '.com.br';
+
+let fullSite = url;
+
+console.log(fullSite);
+//
+const address = 'EQMM 17/55';
+const firstAddress = 'Bloco 8, LT 783 - Groelandia';
+
+let fullAddress = `${address} ${firstAddress}`;
+
+console.log(fullAddress);
