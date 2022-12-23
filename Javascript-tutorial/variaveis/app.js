9e5bc096052e9f2333fafd1618206c132ee66177
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-12-23 09:21:42 -0300

    Criação da pasta e aquivo variaveis/app.js, escopo de variavéis.

---
+++ Javascript-tutorial/variaveis/app.js
@@ -0,0 +1,32 @@
+//Declare, Assigment Operator, Assign Value
+
+let nameComplete = "Thiago shevchenko santos v";
+console.log(nameComplete);
+
+//
+
+//Assign value later, modify existing
+
+let address, zip, state;
+address = "101 main street";
+zip = '5572213';
+state = 'CA';
+address = "1025 street N";
+console.log(address, zip, state);
+
+//LET vs CONST vs VAR
+
+//using VAR
+var value = "some value";
+value = "other value";
+
+//using LET
+let name = "john";
+name = "Jonh silver";
+
+//using CONST (CONSTANT) - can't re-assign
+const lastName = "jordan";
+
+console.log(value);
+console.log(name);
+console.log(lastName);
