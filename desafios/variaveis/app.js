30ee64435637777b678e5e7880932c19743ffe67
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-12-23 10:24:22 -0300

    Criação da pasta de desafios, aquivo index.html e arquivo de desafio de variáveis app.js.

---
+++ desafios/variaveis/app.js
@@ -0,0 +1,8 @@
+const firstName = "Thiago";
+const last_Name = "Santos de Sousa";
+
+let address = "11025, street 023 CA";
+
+address = "new street 0028";
+
+console.log(firstName, last_Name, address);
