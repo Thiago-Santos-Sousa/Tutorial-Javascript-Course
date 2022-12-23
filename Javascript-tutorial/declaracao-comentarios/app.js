90d7f35f304f681760546b0e7c48e31411a1c83f
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-12-22 22:54:38 -0300

    Criando a pasta de declaração e comentários e o arquivo app.js correspondente.

---
+++ Javascript-tutorial/declaracao-comentarios/app.js
@@ -0,0 +1,10 @@
+//I woke up early today
+//declarations - instruction sets
+//coments - shortcurt comand = //
+
+//this is console.log statement
+//console.log('hello world');
+/*console.log('hello people');
+console.log('hello my friend Peter');
+console.log('hello Susan');*/
+document.write('hello Anna');
