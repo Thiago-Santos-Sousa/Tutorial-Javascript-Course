23b1cf2eeebeff2886b683755e1fa51187fb54c0
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-12-26 13:30:44 -0300

    Desafio concatenação
    Criação da pasta e desafio concatenção/app.js, exercitando concatenações simples.

---
+++ desafios/concatenacao/app.js
@@ -0,0 +1,9 @@
+const street = 'Virgin river';
+const country = 'Canada';
+
+let address = street + ' ' + country;
+let myAddress = `${street} ${country}`;
+
+console.log('mailing address: ' + street, country);
+console.log(`mailing address: ${address}`);
+console.log(`mailing address: ${myAddress}`);
