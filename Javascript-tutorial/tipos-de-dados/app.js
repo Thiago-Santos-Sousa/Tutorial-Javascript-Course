92f48f27751931e5f2b9586ef101fc8be9f603f0
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-12-27 10:03:51 -0300

    Tipos de dados
    Criação da pasta e arquivo tipos-de-dados/app.js, tipos primitivos em JS e operador typeof

---
+++ Javascript-tutorial/tipos-de-dados/app.js
@@ -0,0 +1,31 @@
+// Data types - 7 total
+// Primitive - String, Number, Boolean, Null, Undefined, Symbol
+// Object - Arrays, Functions, Objects
+
+// typeof - operator (typeof variable) (typeof value)
+
+// String
+const text = 'some text';
+
+// Number
+const number0 = 40;
+
+// Boolean
+let value0 = true;
+let value1 = false;
+
+// Null
+const result = null;
+
+// Undefined
+let name0;
+
+// Symbol(ES6)
+
+console.log(text);
+console.log(number0);
+console.log(value0, value1);
+console.log(result);
+console.log(name0);
+
+console.log(typeof value0, typeof result, typeof name0);
