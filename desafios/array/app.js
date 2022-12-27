798e61210cc8cd77130301a9f55be47965829779
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-12-27 11:03:15 -0300

    Desafio array
    Criação da pasta e arquivo array/app.js, desafios: criação do array, alteração de indices, chamda e reatribuição de valor.

---
+++ desafios/array/app.js
@@ -0,0 +1,8 @@
+const fruits = ['banana', '', 'litter', 'orange', 'pear'];
+
+fruits[1] = 'noodle';
+let noFruit = fruits[1];
+
+console.log(`in the number 1 index we don't have a fruit but: ${noFruit}`);
+console.log(fruits[4]);
+console.log(fruits);
