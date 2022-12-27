24d9dd039d3f2af618bbc24045c109deec109cda
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-12-27 10:47:45 -0300

    Arrays, indices e chamada
    Criação da pasta e aquivo arrays/app.js, exemplo de array, atribuindo e alterando valores em indices.

---
+++ Javascript-tutorial/arrays/app.js
@@ -0,0 +1,22 @@
+// Arrays, Functions and Objects
+// Arrays - [], 0 index based
+
+const family = [
+  'Thiago Santos',
+  'Thais Sampaio',
+  'Estevão Santos Sampaio',
+  'Thalita Santos',
+  'Lucas Sampaio',
+  ''
+];
+
+console.log(family);
+
+let love = family[1];
+console.log(love);
+
+family[5] = 'Zenilde Nascimentos';
+let mom = family[5];
+console.log(mom);
+
+console.log(family[2]); //access index
