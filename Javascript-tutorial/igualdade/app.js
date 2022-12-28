f36e803fc849e5b3052656b1535270633fe9355d
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-12-28 16:42:08 -0300

    Igualdade
    Criação da pasta e arquivo igualdade/app.js, aplicando operadores de igualdade.

---
+++ Javascript-tutorial/igualdade/app.js
@@ -0,0 +1,17 @@
+// checking value, checking value and type
+// ==, ===, !=, !===
+
+const Num1 = 6;
+const Num2 = '6';
+
+const value0 = Num1 == Num2;
+const value1 = Num1 === Num2;
+
+const value2 = Num1 != Num2;
+const value3 = Num1 !== Num2;
+
+console.log(value0);
+console.log(value1);
+
+console.log(value2);
+console.log(value3);
