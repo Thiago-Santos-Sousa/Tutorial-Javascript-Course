4efaaa3ad51a85da3c03bacc24acdb4d77f092df
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-12-28 17:10:30 -0300

    Operadores Lógicos
    Criação da pasta e arquivo operadores-logicos/app.js, operadores &&, || (AND ou OR.)

---
+++ Javascript-tutorial/operadores-logicos/app.js
@@ -0,0 +1,22 @@
+// Logical Operators
+// (|| - OR), (&& - AND), !
+
+const name = 'bob';
+const age = 45;
+
+if (name === 'bob' && age === 44) {
+  console.log('hello there user');
+} else {
+  console.log('wrong values');
+}
+
+//
+
+const day0 = 's';
+const day1 = 't';
+
+if (day0 === 's' || day1 === 2) {
+  console.log('day true');
+} else {
+  console.log('day false');
+}
