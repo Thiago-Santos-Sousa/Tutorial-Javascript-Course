f016987ab8dd467b144da6af383fdba4e1f8a776
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-12-28 19:28:31 -0300

    Loop Do While
    Criação da pasta e arquivo loop-do-while/app.js, entendendo e aplicando loop.

---
+++ Javascript-tutorial/loop-do-while/app.js
@@ -0,0 +1,19 @@
+// do while loop
+// execute at least once
+
+let money = 0;
+
+do {
+  console.log(`you have ${money} dollars`);
+  money++;
+} while (money < 10);
+
+//
+
+let age = 9;
+
+do {
+  console.log(`I'm ${age} age`);
+  age--;
+} while (age > 10)
+
