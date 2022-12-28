13a95b0c0f26546007e04d26b1d5d87e8e8143d5
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-12-28 19:10:46 -0300

    Loop while
    Criação da pasta e arquivo loop-while/app.js, executando loop e definindo parametros.

---
+++ Javascript-tutorial/loop-while/app.js
@@ -0,0 +1,20 @@
+// Loops
+// while loop
+
+let amount = 10;
+
+while (amount > 0) {
+  console.log(`I have ${amount} dollars and I'm going to the mall`);
+
+  amount--;
+}
+
+//
+
+let number = 0;
+
+while (number < 10) {
+  console.log(`add ${number}`);
+
+  number++;
+}
