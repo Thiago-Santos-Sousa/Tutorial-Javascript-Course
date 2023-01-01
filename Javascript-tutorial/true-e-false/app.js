a91da78d315c7f9df3df55a5f8767447a79e4177
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2023-01-01 11:09:16 -0300

    Truthy e Falsy
    Criação da pasta e arquivo true-e-false/app.js, true e false, retornos e casos de uso.

---
+++ Javascript-tutorial/true-e-false/app.js
@@ -0,0 +1,22 @@
+// True and False
+
+const bool1 = true;
+const bool2 = 2 > 1;
+
+if (bool1) {
+  console.log('Hey the value Truthy');
+}
+
+if (bool2) {
+  console.log('Hey the value Truthy');
+}
+
+//
+
+const text = '';
+
+if (text) {
+  console.log('Hey the value Truthy');
+} else {
+  console.log('Hey the value Falsy');
+}
