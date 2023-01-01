7552e700a2b4a5662a437481d18bdca773836271
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2023-01-01 11:23:39 -0300

    Operador ternario
    Criação da pasta e arquivo operador-ternario/app.js, aplicando operador.

---
+++ Javascript-tutorial/operador-ternario/app.js
@@ -0,0 +1,17 @@
+// Ternary operator
+// condicion ? (runs if true) : (runs if false)
+
+const value = 1 < 0;
+
+value ? console.log('value is true') : console.log('value is false');
+
+
+const names = '';
+
+names ? console.log('value: true') : console.log('value: false');
+
+/*if (value) {
+  console.log('value is true');
+} else {
+  console.log('value is false');
+}*/
