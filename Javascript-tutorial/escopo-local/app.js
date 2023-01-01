8d4487a43401e37184cc4e04e3f9a0c8009a3aba
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2023-01-01 16:57:42 -0300

    Escopo Local
    Criação da pasta e arquivo escopo-local/app.js, aplicando variações de escopo local.

---
+++ Javascript-tutorial/escopo-local/app.js
@@ -0,0 +1,19 @@
+// Local scope
+
+let name = 'Thiago';
+
+console.log(name);
+
+function otherName() {
+  const lastName = 'sebastiao';
+  const age = 27;
+  AllName = `Thiago ${lastName}, age: ${age}`;
+}
+
+otherName();
+console.log(AllName);
+
+if (true) {
+  const name = 'jhon';
+  console.log(`true, name is ${name}`);
+}
