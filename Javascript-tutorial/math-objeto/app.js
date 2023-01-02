f41dc2664f4e8934704783204a7d3c6990fee443
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2023-01-02 18:21:13 -0300

    Math objeto
     Criação da pasta e arquivo math-objeto/app.js, exempleficando objetos math.

---
+++ Javascript-tutorial/math-objeto/app.js
@@ -0,0 +1,19 @@
+// Math
+
+const number = 4.58452;
+const result = Math.floor(number);
+
+const number1 = 4.11225;
+const result1 = Math.ceil(number1);
+
+const number2 = 80;
+const result2 = Math.sqrt(number2);
+
+const result3 = Math.PI;
+
+const result4 = Math.min(4, 5, 6, 9);
+
+const result5 = Math.max(4, 8, 10, 23, 147);
+
+const result6 = Math.floor(Math.random() * 10);
+console.log(result6);
