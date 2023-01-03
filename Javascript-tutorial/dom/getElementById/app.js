395c74afb5803c6ad8afd6abb17096ab0f65d491
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2023-01-03 11:56:19 -0300

    getElemntById
    Aplicando metodo getElemntbyId.

---
+++ Javascript-tutorial/dom/getElementById/app.js
@@ -0,0 +1,14 @@
+// select the element or group of elements the we want
+// decide the effect we want to apply to theselection
+
+//document.getElementById('element')
+
+const h1 = document.getElementById('title');
+h1.style.color = 'red';
+
+const btn = document.getElementById('btn');
+
+btn.style.color = 'red';
+btn.style.backgroundColor = 'blue';
+//document.getElementById('btn').style.backgroundColor = 'blue';
+//document.getElementById('btn').style.color = 'white';
