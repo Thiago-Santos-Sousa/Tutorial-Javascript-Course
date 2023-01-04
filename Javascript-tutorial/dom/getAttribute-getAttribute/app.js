963da387074b13a4ce0bf58e741da014812280fe
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2023-01-04 19:07:01 -0300

    SetAttribute() e GetAttribute()
    Criação da pasta e arquivo setAttribute-getAttribute/app.js, aplicando propriedade.

---
+++ Javascript-tutorial/dom/getAttribute-getAttribute/app.js
@@ -0,0 +1,18 @@
+// SetAttribute();
+// GetAttribute();
+
+const first = document.querySelector('.first');
+const idValue = first.getAttribute('id');
+//console.log(idValue);
+
+const link = document.getElementById('link');
+const showLink = link.getAttribute('href');
+console.log(showLink);
+
+const last = link.nextElementSibling;
+last.setAttribute('class', 'first');
+last.textContent = 'i also have a classof first';
+console.log(last);
+
+const links = document.querySelector('.first');
+console.log(links);
