9581deb36adf2de11a9903b1e68a929d81ab7bcd
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2023-01-04 08:05:48 -0300

    getElementsByClassName
    Criação da pasta e arquivo getElementsByClassName/app.js, aplicando metodo e exemplificando.

---
+++ Javascript-tutorial/dom/getElementsByClassName/app.js
@@ -0,0 +1,10 @@
+// getElementsByClassName('className);
+
+//HTMLCollection
+
+const listNumber = document.getElementsByClassName('especial');
+listNumber[1].style.color = 'red';
+console.log(listNumber);
+
+const title = document.getElementsByClassName('title');
+console.log(title);
