5b968e29bd64cf9518901743285c0b32a667b005
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2023-01-06 21:04:41 -0300

    Prepend e InnerText
    Criação da pasta e arquivo prepend-innerText/app.js/index.html, aplicando metodos e propriedades.

---
+++ Javascript-tutorial/dom/prepend-innerText/app.js
@@ -0,0 +1,12 @@
+// prepend
+// innerText
+
+const heading = document.createElement('h2');
+heading.innerText = 'i am a dynamic heading';
+document.body.prepend(heading);
+
+//
+
+const text = document.createElement('h2');
+text.innerText = 'My text dynamic';
+document.body.prepend(text);
