1f33c6f94a3387584132d0dac302cf08be162bf6
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2023-01-10 10:56:20 -0300

    Events Scroll
    Criação da pasta e arquivo events-scroll/app.js/index.html, aplicando metodos.

---
+++ Javascript-tutorial/dom/events-scroll/app.js
@@ -0,0 +1,7 @@
+// Scroll Event - on element of document
+
+window.addEventListener('scroll', function () {
+  //console.log('hello');
+  console.log(window.scrollY + 'px');
+  console.log(window.scrollX + 'px');
+});
