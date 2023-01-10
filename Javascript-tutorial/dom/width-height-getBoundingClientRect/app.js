3891f7e5ee9c39cbbca4889ffaa6adf23f0047f1
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2023-01-10 11:24:20 -0300

    Widht, height - getBoundingClientRect
    Aplicando metodos e propriedades, Criação da pasta e arquivo widht-height-getBoundingClientRect/app.js/index.html.

---
+++ Javascript-tutorial/dom/width-height-getBoundingClientRect/app.js
@@ -0,0 +1,10 @@
+console.log('heigth : ' + window.innerHeight);
+console.log('width : ' + window.innerWidth);
+
+const btn = document.querySelector('.btn');
+const box = document.querySelector('.box');
+
+btn.addEventListener('click', function () {
+  const values = box.getBoundingClientRect();
+  console.log(values);
+});
